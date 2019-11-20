3610039c341b73116f5471770536f17a6a57ce0e
gmaranon <gary@pinacourt.com>
2019-11-20 00:17:17 -0400

    FOOTER, header, CHATS - Botones flotantes

--- wp-content/plugins/chat_flotante/includes/widget.php
+++ wp-content/plugins/chat_flotante/includes/widget.php
@@ -18,27 +18,41 @@ class chat_flotante extends WP_Widget {
     public function widget( $args, $instance ) {
         ?>
         <div class="chat_flotante_wsp_messenger">
-            <a href="https://api.whatsapp.com/send?phone=13059274105" class="whatsapp" target="_blank">
+            <a href="https://api.whatsapp.com/send?phone=<?php echo  $instance["whatsapp_input"];?>" class="whatsapp" target="_blank">
                 <img alt="WHATSAPP CHAT" src="<?php echo plugin_dir_url( __FILE__ ) . '../assets/images/whatsapp_chatweb.png' ?>" />
             </a>
 
-            <a href="https://m.me/goliquidator" class="messenger" target="_blank" onclick="window.open(this.href, this.target, 'width=900,height=600,top=100,left=300,scrollbars=NO,menubar=NO,titlebar= NO,status=NO,toolbar=NO'); return false;">
+            <a href="https://m.me/<?php echo  $instance["messenger_input"];?>" class="messenger" target="_blank" onclick="window.open(this.href, this.target, 'width=900,height=600,top=100,left=300,scrollbars=NO,menubar=NO,titlebar= NO,status=NO,toolbar=NO'); return false;">
                 <img alt="MESSENGER CHAT" src="<?php echo plugin_dir_url( __FILE__ ) . '../assets/images/messenger_chatweb.png' ?>" />
             </a>
         </div>
-
-
 <?php
 
     }
 
     // output the option form field in admin Widgets screen
     public function form( $instance ) {
-
+?>
+        <p>
+            <label for="<?php echo $this->get_field_id('whatsapp_input'); ?>">Whatsapp Number</label>
+            <input class="widefat" id="<?php echo $this->get_field_id('whatsapp_input'); ?>" name="<?php echo $this->get_field_name('whatsapp_input'); ?>" type="text" value="<?php echo esc_attr($instance["whatsapp_input"]); ?>" />
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('messenger_input'); ?>">Messenger Code</label>
+            <input class="widefat" id="<?php echo $this->get_field_id('messenger_input'); ?>" name="<?php echo $this->get_field_name('messenger_input'); ?>" type="text" value="<?php echo esc_attr($instance["messenger_input"]); ?>" />
+        </p>
+  <?php
     }
 
     // save options
-    public function update( $new_instance, $old_instance ) {}
+    public function update( $new_instance, $old_instance ) {
+
+        $instance = $old_instance;
+        $instance["whatsapp_input"] = strip_tags($new_instance["whatsapp_input"]);
+        $instance["messenger_input"] = strip_tags($new_instance["messenger_input"]);
+        return $instance;
+
+    }
 }
 
 ?>
