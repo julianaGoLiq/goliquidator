98f97b5688aff256f0e3dca49b2f1f9460e46c5a
Gary Marañon <gary.maranon@onetree.com>
2021-01-22 14:59:23 -0400

    fix error

--- wp-content/themes/suprema-child/functions.php
+++ wp-content/themes/suprema-child/functions.php
@@ -2,15 +2,15 @@
 
 /*** Child Theme Function  ***/
 if ( ! function_exists( 'suprema_qodef_child_theme_enqueue_scripts' ) ) {
-	function suprema_qodef_child_theme_enqueue_scripts()
-	{
+    function suprema_qodef_child_theme_enqueue_scripts()
+    {
 
-		$parent_style = 'suprema-qodef-default-style';
+        $parent_style = 'suprema-qodef-default-style';
 
-		wp_enqueue_style('suprema-qodef-child-style', get_stylesheet_directory_uri() . '/style.css', array($parent_style));
-	}
+        wp_enqueue_style('suprema-qodef-child-style', get_stylesheet_directory_uri() . '/style.css', array($parent_style));
+    }
 
-	add_action('wp_enqueue_scripts', 'suprema_qodef_child_theme_enqueue_scripts');
+    add_action('wp_enqueue_scripts', 'suprema_qodef_child_theme_enqueue_scripts');
 }
 
 /*
